79661f77aee25fc74fbbcb318bd9b65e6b35112f
Allen Li <liyuchen223@gmail.com>
2024-07-23 09:23:35 -0400

    merge issue
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- internal/controller/accountiam_controller.go
+++ internal/controller/accountiam_controller.go
@@ -89,10 +89,7 @@ var BootstrapData BootstrapSecret
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
 // the AccountIAM object against the actual cluster state, and then
-// perform operations to make the cluster state 
-
-
-the state specified by
+// perform operations to make the cluster state reflect the state specified by
 // the user.
 //
 // For more details, check Reconcile and its Result here:
