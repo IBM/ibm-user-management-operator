1b294ea15ff27c850f5387e282582799e054ca3b
Daniel Fan <fanyuchensx@gmail.com>
2025-10-14 12:02:37 -0700

    Fix variable name from VERSION to BUILD_VERSION in multiarch_image.sh
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>

--- common/scripts/multiarch_image.sh
+++ common/scripts/multiarch_image.sh
@@ -16,13 +16,13 @@
 #
 
 # This script build and push multiarch(amd64, ppc64le and s390x) image for the one specified by
-# IMAGE_REPO, IMAGE_NAME and VERSION.
+# IMAGE_REPO, IMAGE_NAME and BUILD_VERSION.
 # It assumes the specified image for each platform is already pushed into corresponding docker registry.
 
 ALL_PLATFORMS="amd64 ppc64le s390x"
 
 IMAGE_TAG_BASE=${1}
-VERSION=${2-"$(git describe --exact-match 2> /dev/null || git describe --match=$(git rev-parse --short=8 HEAD) --always --dirty --abbrev=8)"}
+BUILD_VERSION=${2-"$(git describe --exact-match 2> /dev/null || git describe --match=$(git rev-parse --short=8 HEAD) --always --dirty --abbrev=8)"}
 RELEASE_VERSION=${3}
 MAX_PULLING_RETRY=${MAX_PULLING_RETRY-10}
 RETRY_INTERVAL=${RETRY_INTERVAL-10}
@@ -35,11 +35,11 @@ for arch in ${ALL_PLATFORMS}
 do
     for i in $(seq 1 "${MAX_PULLING_RETRY}")
     do
-        echo "Checking image '${IMAGE_TAG_BASE}'-'${arch}':'${VERSION}'..."
-        ${CONTAINER_CLI} manifest inspect "${IMAGE_TAG_BASE}"-"${arch}":"${VERSION}" && break
+        echo "Checking image '${IMAGE_TAG_BASE}'-'${arch}':'${BUILD_VERSION}'..."
+        ${CONTAINER_CLI} manifest inspect "${IMAGE_TAG_BASE}"-"${arch}":"${BUILD_VERSION}" && break
         sleep "${RETRY_INTERVAL}"
         if [ "${i}" -eq "${MAX_PULLING_RETRY}" ]; then
-            echo "Failed to found image '${IMAGE_TAG_BASE}'-'${arch}':'${VERSION}'!!!"
+            echo "Failed to found image '${IMAGE_TAG_BASE}'-'${arch}':'${BUILD_VERSION}'!!!"
             exit 1
         fi
     done
@@ -48,14 +48,14 @@ done
 # create multi-arch manifest
 echo "Creating the multi-arch image manifest for ${IMAGE_TAG_BASE}:${RELEASE_VERSION}..."
 ${CONTAINER_CLI} manifest create "${IMAGE_TAG_BASE}":"${RELEASE_VERSION}" \
-    "${IMAGE_TAG_BASE}"-amd64:"${VERSION}" \
-    "${IMAGE_TAG_BASE}"-ppc64le:"${VERSION}" \
-    "${IMAGE_TAG_BASE}"-s390x:"${VERSION}"
+    "${IMAGE_TAG_BASE}"-amd64:"${BUILD_VERSION}" \
+    "${IMAGE_TAG_BASE}"-ppc64le:"${BUILD_VERSION}" \
+    "${IMAGE_TAG_BASE}"-s390x:"${BUILD_VERSION}"
 echo "Creating the multi-arch image manifest for ${IMAGE_TAG_BASE}:latest..."
 ${CONTAINER_CLI} manifest create "${IMAGE_TAG_BASE}":latest \
-    "${IMAGE_TAG_BASE}"-amd64:"${VERSION}" \
-    "${IMAGE_TAG_BASE}"-ppc64le:"${VERSION}" \
-    "${IMAGE_TAG_BASE}"-s390x:"${VERSION}"
+    "${IMAGE_TAG_BASE}"-amd64:"${BUILD_VERSION}" \
+    "${IMAGE_TAG_BASE}"-ppc64le:"${BUILD_VERSION}" \
+    "${IMAGE_TAG_BASE}"-s390x:"${BUILD_VERSION}"
 
 # push multi-arch manifest
 echo "Pushing the multi-arch image manifest for ${IMAGE_TAG_BASE}:${RELEASE_VERSION}..."
